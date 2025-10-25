2f369101e912a2d690204c8725b6523714959c70
bolivier <2995224+bolivier@users.noreply.github.com>
2025-10-25 15:18:19 +0000

    [create-pull-request] automated change

--- Formula/tram.rb
+++ Formula/tram.rb
@@ -1,2 +1,22 @@
 class Tram < Formula
+  desc "A web framework written in Clojure"
+  homepage "https://github.com/bolivier/tram"
+  url "https://github.com/bolivier/tram/releases/download/v0.0.1/tram.tar.gz"
+  sha256 "e0b202f355f1c3a228f27e6ce50d5c26ac44d2fd82b9f3cb942fa4c6e31ad2ac"
+  license "MIT"
+
+  depends_on "borkdude/brew/babashka"
+  depends_on "clojure"
+
+  def install
+    libexec.install "src"
+    libexec.install "bb.edn"
+    bin.install "tram"
+  end
+
+
+
+  test do
+    system "#{bin}/wtm", "--help"
+  end
 end
