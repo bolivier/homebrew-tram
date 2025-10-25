a7f7e45494a31bbb353b097c880e6acaeb3ea601
Brandon Olivier <brandon@brandonolivier.com>
2025-10-25 09:44:43 -0500

    Move to tram data

---
+++ Formula/tram.template.rb
@@ -0,0 +1,22 @@
+class Tram < Formula
+  desc "A web framework written in Clojure"
+  homepage "https://github.com/bolivier/tram"
+  url "https://github.com/bolivier/tram/releases/download/__VERSION__/tram.tar.gz"
+  sha256 "__SHA__"
+  license "MIT"
+
+  depends_on "borkdude/brew/babashka"
+  depends_on "clojure"
+
+  def install
+    libexec.install "src"
+    libexec.install "bb.edn"
+    bin.install "tram"
+  end
+
+
+
+  test do
+    system "#{bin}/wtm", "--help"
+  end
+end
